6780940b57702be3b6c0c805ebbd0a577fbfcf6d
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-27 20:44:10 +0800

    Code Update: Completed writing the code to get the sum of an array of numbers

--- problem-1/problem-1.js
+++ problem-1/problem-1.js
@@ -1,8 +1,13 @@
 function sumOfNumbers(array) {
-  // input type checking
   if (!Array.isArray(array)) {
     throw new Error("Input type is not a type of 'Array'");
   }
+
+  let sum = 0;
+  for (let number = 0; number < array.length; number++) {
+    sum = sum + array[number];
+  }
+  return sum;
 }
 
 const array = [2, 5, 10, 23, 1, 8];
