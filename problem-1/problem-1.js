261707685b3dcbac87df75290e78ccdc4b5b74b2
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-27 19:53:37 +0800

    Code Update: Added an input type checking on the sumOfNumbers() function

--- problem-1/problem-1.js
+++ problem-1/problem-1.js
@@ -1,5 +1,8 @@
 function sumOfNumbers(array) {
   // input type checking
+  if (!Array.isArray(array)) {
+    throw new Error("Input type is not a type of Array");
+  }
 }
 
 const array = [2, 5, 10, 23, 1, 8];
