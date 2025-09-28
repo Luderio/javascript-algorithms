104a17ff8981b3820f87f55981185cd501cb489f
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-28 10:32:50 +0800

    Code Update: Added Input Type checking and arithmetic input checking

--- problem-3/problem-3.js
+++ problem-3/problem-3.js
@@ -1,4 +1,15 @@
-const sumOfArithmetic = (numbers) => {};
+import isArithmetic from "../problem-2/problem-2"; // imported isArithmetic() function from previous code challenge
+
+const sumOfArithmetic = (numbers) => {
+  // checking the input type
+  if (!Array.isArray(numbers)) {
+    throw new Error("Input type must be a type of 'Array'");
+  }
+  // check if the input array follows an arithmetic pattern
+  if (!isArithmetic(numbers)) {
+    throw new Error("Input must be an arithmetic array");
+  }
+};
 
 const numbers = [2, 7, 12, 17, 22, 27, 32, 37, 42];
 const sum = sumOfArithmetic(numbers);
