df691d1d05554233cd65f75839e7c8bd359e4d96
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-28 09:42:47 +0800

    Code Update: Completed the algorithm to check if the array is an arithmetic array or not

--- problem-2/problem-2.js
+++ problem-2/problem-2.js
@@ -9,14 +9,3 @@ const isArithmetic = (array) => {
     return true;
   }
 };
-
-const sumOfArithmetic = (numbers) => {
-  // checking the input type
-  if (!Array.isArray(numbers)) {
-    throw new Error("Input type must be a type of 'Array'");
-  }
-};
-
-const numbers = [2, 7, 12, 17, 22, 27, 32, 37, 42];
-const sum = sumOfArithmetic(numbers);
-console.log(sum);
