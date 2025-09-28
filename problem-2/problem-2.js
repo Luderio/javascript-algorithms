14ea1c858c3eefcb2ca1b6536c1f9cdb49f72cc3
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-28 09:37:28 +0800

    Code Update: Created a function called isArithmetic() to check if the input array on the main function is an arithmetic array of not

--- problem-2/problem-2.js
+++ problem-2/problem-2.js
@@ -1,4 +1,21 @@
-const sumOfArithmetic = (numbers) => {};
+const isArithmetic = (array) => {
+  if (!Array.isArray(array) || array.length < 2) return false;
+
+  // calculates the difference for later comparisons.
+  let difference = array[1] - array[0];
+
+  for (let i = 2; i < array.length; i++) {
+    if (array[i] - array[i - 1] !== difference) return false;
+    return true;
+  }
+};
+
+const sumOfArithmetic = (numbers) => {
+  // checking the input type
+  if (!Array.isArray(numbers)) {
+    throw new Error("Input type must be a type of 'Array'");
+  }
+};
 
 const numbers = [2, 7, 12, 17, 22, 27, 32, 37, 42];
 const sum = sumOfArithmetic(numbers);
