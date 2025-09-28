ae644bca528b996e8ac4e81dcb99faea57f1d20b
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-28 11:18:39 +0800

    Code Update: Completed the second version of the sumOfArithmetic() function

--- problem-3/problem-3.js
+++ problem-3/problem-3.js
@@ -9,24 +9,44 @@ const isArithmetic = (array) => {
   return true;
 };
 
+// const sumOfArithmetic = (numbers) => {
+//   // checking the input type
+//   if (!Array.isArray(numbers)) {
+//     throw new Error("Input type must be a type of 'Array'");
+//   }
+//   // check if the input array follows an arithmetic pattern
+//   if (!isArithmetic(numbers)) {
+//     throw new Error("Input must be an arithmetic array");
+//   }
+
+//   // will use the mathematical formula: sum = (n / 2) * (first + last) on getting the sum of the arithmetic sequence.
+//   const n = numbers.length;
+//   const first = numbers[0];
+//   const last = numbers[n - 1];
+
+//   // returns the result of the formula
+//   const sum = (n / 2) * (first + last);
+//   return sum;
+// };
+
+// this verssion has a Big o Notation: O(3 + 1n + 1n) or O(2n) or O(n) / Linear Time Complexity.
 const sumOfArithmetic = (numbers) => {
-  // checking the input type
+  // checking the input type. Will execute 1 time.
   if (!Array.isArray(numbers)) {
     throw new Error("Input type must be a type of 'Array'");
   }
-  // check if the input array follows an arithmetic pattern
+  // check if the input array follows an arithmetic pattern. Will execute n times (depending on the length of the input: numbers) + 1.
   if (!isArithmetic(numbers)) {
     throw new Error("Input must be an arithmetic array");
   }
 
-  // will use the mathematical formula: sum = (n / 2) * (first + last) on getting the sum of the arithmetic sequence.
-  const n = numbers.length;
-  const first = numbers[0];
-  const last = numbers[n - 1];
+  let sum = 0; // Will execute 1 time.
 
-  // returns the result of the formula
-  const sum = (n / 2) * (first + last);
-  return sum;
+  // Will execute n times (depending on the length of the input: numbers).
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum; // Will execute 1 time.
 };
 
 const numbers = [2, 7, 12, 17, 22, 27, 32, 37, 42];
