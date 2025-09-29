ccca59452dcab97a233582df5346da3a54acb3de
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-29 17:25:04 +0800

    Code Update: Finalized the Initial Fibonacci sequence

--- problem-4/problem-4.js
+++ problem-4/problem-4.js
@@ -3,4 +3,13 @@ function fib(n) {
   if (typeof n !== "number") {
     throw new Error("input must be a type of 'number'");
   }
+
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
 }
+
+const fibonacci = fib(0);
+console.log(fibonacci);
