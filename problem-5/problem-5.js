183c068dfa59529dcb4f5d4decab6b1b647d76bc
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-30 20:51:09 +0800

    Code Update: Completed the core logic for the gridTraveler problem.

--- problem-5/problem-5.js
+++ problem-5/problem-5.js
@@ -3,6 +3,14 @@ function gridTraveler(m, n) {
   if (typeof m !== "number" && typeof n !== "number") {
     throw new Error("input must be a type of 'Number'");
   }
+
+  if (m === 0 || n === 0) return 0;
+  if (m === 1 && n === 1) return 1;
+
+  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
 }
 
+console.log(gridTraveler(2, 3));
 console.log(gridTraveler(4, 4));
+console.log(gridTraveler(6, 6));
+console.log(gridTraveler(10, 10));
