41bcfcdba97f9a85fe975917cdfd78e52eeadb90
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-09-30 16:33:48 +0800

    Save Point: Saving the version of fibonacci algorithm with memoization

--- problem-4/problem-4.js
+++ problem-4/problem-4.js
@@ -1,17 +1,30 @@
-// classic fibonacci algorithm.
-function fib(n) {
+/**
+ * Memoization using javascript object
+ * Memoization is used to optimize and speed up the fibonacci algorithm specifically in larger fibonacci sequence.
+ * will add a new argument on the fib() function that is an object.
+ * object keys will be the first argument of fib().
+ * object value will be the return value.
+ */
+
+function fib(n, memo = {}) {
   // input type checking
   if (typeof n !== "number") {
     throw new Error("input must be a type of 'number'");
   }
+
+  // checks if the input n is already added in the memo object.
+  if (n in memo) return memo[n];
 
   if (n < 2) {
     return n;
   }
 
   // fibonacci formula: fib = fib(n - 1) + fib(n - 2) for n >= 2.
-  return fib(n - 1) + fib(n - 2);
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
 }
 
-const fibonacci = fib(0);
-console.log(fibonacci);
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+console.log(fib(50));
