771ac65eacf7cc6bb0bd32977d048d766ea86478
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-01 18:19:14 +0800

    Code Update: Added an alternative way on solving the problem using the Binomial Coeficient formula

--- problem-5/problem-5.js
+++ problem-5/problem-5.js
@@ -56,3 +56,25 @@
 // console.log(gridTraveler(50, 50)); // 254,776,122,589,808,670,000,000,000,000.
 
 //=========================================================================================
+
+/**
+ * Using Binomial Coeficient formula
+ *
+ */
+
+function gridTraveler(m, n) {
+  // input type check
+  if (typeof m !== "number" && typeof n !== "number") {
+    throw new Error("input must be a type of 'Number'");
+  }
+
+  if (m === 0 || n === 0) return 0;
+  if (m === 1 && n === 1) return 1;
+  return gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+}
+
+console.log(gridTraveler(2, 3)); // 3
+console.log(gridTraveler(4, 4)); // 20
+console.log(gridTraveler(6, 6)); // 252
+console.log(gridTraveler(10, 10)); // 48620
+console.log(gridTraveler(50, 50)); // 254,776,122,589,808,670,000,000,000,000.
