7695b548661c8eeca4bc54a485ee1cef3c126114
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-02 21:57:25 +0800

    Code Update: Modified the file to store the second version of the gridTraveler algorithm

--- problem-5/problem-5.1.js
+++ problem-5/problem-5.1.js
@@ -1,20 +1,26 @@
 /**
- * Core Algorithm without Memoization
- * Will recursively call and resolve every **_gridTraveler(m, n)_** calls even the calls with the same return value (duplicated items).
- * Will become very slow in larger **_m,n_** grids.
- * Has **Big O Time Complexity** of **_O(2^n+m)_** or **_O(2<sup>n+m</sup>)_** or **_Exponential Time Complexity_**.
- * Has **Big O Space Complexity** of **_O(m + n) call stack_** or **_Linear Space Complexity / Linear Call Stack Depth_**.
+ * Core Algorithm with Memoization logic. (Optimized Version).
+ * Adds a new argument with default value: memo = {} which is an object.
+ * Every result gets added to the memo object and the memo value is being passed down by reference to the recursive calls to act as a cache of the result.
+ * At every iteration, the algorithm will check if the key,value pair is already present in the object. if yes, the stored result is used immediately cuttting down on execution time.
+ * Has **Big O Time Complexity** of **_O(n\*m)_** or **_Quadtratic Time Complexity_**.
  */
 
-function gridTraveler(m, n) {
+function gridTraveler(m, n, memo = {}) {
   // input type check
   if (typeof m !== "number" && typeof n !== "number") {
     throw new Error("input must be a type of 'Number'");
   }
 
+  const key = `${m},${n}`;
+  if (key in memo) return memo[key];
+
   if (m === 0 || n === 0) return 0;
   if (m === 1 && n === 1) return 1;
-  return gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+
+  memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+
+  return memo[key];
 }
 
 console.log(gridTraveler(2, 3)); // 3
