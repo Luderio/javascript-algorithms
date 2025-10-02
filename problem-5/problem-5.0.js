f950cba3a720e483319de22465b5d143d0df7bab
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-02 21:57:01 +0800

    Code Update: Modified the file to store the first version of the gridTraveler algorithm

--- problem-5/problem-5.0.js
+++ problem-5/problem-5.0.js
@@ -1,29 +1,20 @@
 /**
- * Using Binomial Coeficient formula (My Preferred version)
- * Follows this binomial Coeficient (Multiplicative Form) formula: C(total, k) = (from i = 1 to k) (total - k + i) / i
- * The formula just needs to be tweaked to: C(total - 2, k - 1) = (from i = 1 to k) (total - k + i) / i to handle proper grid-path counting.
- * The formula will be: C(m + n - 2, n - 1) = (from i = 1 to n) (total - n + i) / i or C(m + n - 2, m - 1) = (from i = 1 to m) (total - m + i) / i (which ever axis (m or n) is smaller)
- * Has Big O Time Complexity of: O(min(m, n)) or Linear Time Complexity
- * Has Big O Space Complexity of: O(1) or Constant Space Complexity
+ * Core Algorithm without Memoization
+ * Will recursively call and resolve every **_gridTraveler(m, n)_** calls even the calls with the same return value (duplicated items).
+ * Will become very slow in larger **_m,n_** grids.
+ * Has **Big O Time Complexity** of **_O(2^n+m)_** or **_O(2<sup>n+m</sup>)_** or **_Exponential Time Complexity_**.
+ * Has **Big O Space Complexity** of **_O(m + n) call stack_** or **_Linear Space Complexity / Linear Call Stack Depth_**.
  */
 
 function gridTraveler(m, n) {
-  if (typeof m !== "number" || typeof n !== "number") {
-    throw new Error("Inputs must be numbers");
+  // input type check
+  if (typeof m !== "number" && typeof n !== "number") {
+    throw new Error("input must be a type of 'Number'");
   }
 
   if (m === 0 || n === 0) return 0;
   if (m === 1 && n === 1) return 1;
-
-  const total = m + n - 2; // to accomodate the move restriction (down, and right or m - 1, and n - 1).
-  const k = Math.min(m - 1, n - 1);
-  let result = 1;
-
-  for (let i = 1; i <= k; i++) {
-    result = (result * (total - k + i)) / i;
-  }
-
-  return result;
+  return gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
 }
 
 console.log(gridTraveler(2, 3)); // 3
