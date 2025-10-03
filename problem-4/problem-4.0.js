254e67936028f89a2f618eb633e4d30abb976037
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-03 20:17:27 +0800

    Code Update: Moved the classic fibonacci sequence logic to problem-4.0.js file

--- problem-4/problem-4.0.js
+++ problem-4/problem-4.0.js
@@ -1,31 +1,16 @@
-/**
- * Memoization using javascript object
- * Memoization is used to optimize and speed up the fibonacci algorithm specifically in larger fibonacci sequence.
- * will add a new argument on the fib() function that is an object fib(n, memo = {}).
- * object keys will be the first argument of fib().
- * object value will be the return value.
- */
+// classic fibonacci implementation (Zero-based)
 
-function fib(n, memo = {}) {
-  // input type checking
-  if (typeof n !== "number") {
-    throw new Error("input must be a type of 'number'");
-  }
-
-  // checks if the input n is already added in the memo object.
-  if (n in memo) return memo[n];
-
+function fib(n) {
   if (n < 2) {
     return n;
   }
 
   // fibonacci formula: fib = fib(n - 1) + fib(n - 2) for n >= 2.
-  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
-  return memo[n];
+  return fib(n - 1) + fib(n - 2);
 }
 
-console.log(fib(6));
-console.log(fib(7));
-console.log(fib(8));
-console.log(fib(50));
-console.log(fib(100));
+console.log(fib(6)); // 8, fast
+console.log(fib(7)); // 13, fast
+console.log(fib(8)); // 21, fast
+console.log(fib(50)); // 12586269025, will be slow
+console.log(fib(100)); // 354224848179262000000, will be slow
