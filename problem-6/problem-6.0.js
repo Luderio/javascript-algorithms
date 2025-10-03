dc28d212a400a7bf983295271d26525cb18ae9b5
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-04 07:43:41 +0800

    Code Update: Added a base code for the cosing problem

---
+++ problem-6/problem-6.0.js
@@ -0,0 +1,12 @@
+/**
+ * Core Algorithm without Memoization.
+ *
+ */
+
+function canSum(targetSum, numbers) {
+  return false;
+}
+
+const numbers = [5, 3, 4, 7];
+const targetSum = 7;
+console.log(canSum(targetSum, numbers));
