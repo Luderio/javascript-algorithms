0279866d0a577eb7282bd6a96a57b3764e6098b9
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-05 06:39:59 +0800

    Code Update: Added comments for documentation

--- problem-6/problem-6.0.js
+++ problem-6/problem-6.0.js
@@ -31,8 +31,8 @@ function canSum(targetSum, numbers) {
   return false;
 }
 
-console.log(canSum(7, [2, 3]));
-console.log(canSum(7, [7, 3, 4, 7]));
-console.log(canSum(7, [2, 4]));
-console.log(canSum(8, [2, 3, 5]));
-console.log(canSum(300, [7, 14]));
+console.log(canSum(7, [2, 3])); // fast
+console.log(canSum(7, [7, 3, 4, 7])); // fast
+console.log(canSum(7, [2, 4])); // fast
+console.log(canSum(8, [2, 3, 5])); // fast
+console.log(canSum(300, [7, 14])); // slow
