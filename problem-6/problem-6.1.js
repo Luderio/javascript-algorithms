8b12ce858d8b8eb1b47f2274e45209369bf456bc
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-05 06:36:10 +0800

    Code Update: Moved the canSum algorithm with memoization to problem-6.1.js file

---
+++ problem-6/problem-6.1.js
@@ -0,0 +1,40 @@
+/**
+ * Core Algorithm without Memoization.
+ *
+ */
+
+function canSum(targetSum, numbers, memo = {}) {
+  // input type checking.
+
+  if (typeof targetSum !== "number") {
+    throw new Error(
+      "argument 'targetSum' must be a type of 'Number' and a non-negative number."
+    );
+  }
+
+  if (!Array.isArray(numbers)) {
+    throw new Error(
+      "argument 'numbers' must be an array of non-negative numbers."
+    );
+  }
+
+  if (targetSum in memo) return memo[targetSum];
+  if (targetSum === 0) return true;
+  if (targetSum < 0) return false;
+
+  for (let num of numbers) {
+    const difference = targetSum - num;
+    if (canSum(difference, numbers, memo) === true) {
+      memo[targetSum] = true;
+      return true;
+    }
+  }
+  memo[targetSum] = false;
+  return false;
+}
+
+console.log(canSum(7, [2, 3]));
+console.log(canSum(7, [7, 3, 4, 7]));
+console.log(canSum(7, [2, 4]));
+console.log(canSum(8, [2, 3, 5]));
+console.log(canSum(300, [7, 14]));
