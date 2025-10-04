c39d473c5bb4b21de3a4a1055622bef8be620b69
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-05 06:35:20 +0800

    Code Update: Reverted the code on the problem-6.0.js file to the brute force version

--- problem-6/problem-6.0.js
+++ problem-6/problem-6.0.js
@@ -3,7 +3,7 @@
  *
  */
 
-function canSum(targetSum, numbers, memo = {}) {
+function canSum(targetSum, numbers) {
   // input type checking.
 
   if (typeof targetSum !== "number") {
@@ -18,18 +18,15 @@ function canSum(targetSum, numbers, memo = {}) {
     );
   }
 
-  if (targetSum in memo) return memo[targetSum];
   if (targetSum === 0) return true;
   if (targetSum < 0) return false;
 
   for (let num of numbers) {
     const difference = targetSum - num;
     if (canSum(difference, numbers, memo) === true) {
-      memo[targetSum] = true;
       return true;
     }
   }
-  memo[targetSum] = false;
   return false;
 }
 
