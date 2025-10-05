ac0867b0fc0333de3ae2611471834910c3da4985
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-05 08:54:09 +0800

    Code Update: Completed my own version of the howSum Algorithm

--- problem-7/problem-7.0.js
+++ problem-7/problem-7.0.js
@@ -9,12 +9,31 @@ function howSum(targetSum, numbers) {
     throw new Error("argument should be a type of 'number'.");
   }
 
-  if (!Array.isArray(numbers)) {
+  if (!Array.isArray(numbers) || numbers === undefined) {
     throw new Error("argument must be an array of numbers.");
   }
 
-  return [];
+  const combinations = [];
+  const current = numbers.shift();
+  const next = numbers[0];
+
+  if (targetSum === 0) return [];
+  if (numbers.length === 0 || targetSum < 0) return null;
+  if (current === targetSum) return current;
+  if (current + next === targetSum) return [current, next];
+
+  let sum = 0;
+  while (sum < targetSum) {
+    combinations.push(current);
+    sum += current;
+  }
+  if (sum === targetSum) return combinations;
+
+  return howSum(targetSum, numbers);
 }
 
 console.log(howSum(7, [5, 3, 4, 7])); // should return  [3, 4] or [7]
 console.log(howSum(8, [2, 3, 5])); // should return [2, 2, 2, 2]  or [3, 5]
+console.log(howSum(7, [2, 4])); // should return null
+console.log(howSum(0, [1, 2, 3])); // []
+console.log(howSum(300, [1, 2, 3]));
