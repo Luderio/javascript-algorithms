a6733488d450af6c82ddac1f68690fa3a2ea3486
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-05 12:00:14 +0800

    Code Update: Modified some code on the file

--- problem-7/problem-7.0.js
+++ problem-7/problem-7.0.js
@@ -1,6 +1,6 @@
 /**
  * Core Algorithm (My Own Version) - Unoptimized
- * Big O Time Complexity: O(n^m) or Exponential Time Complexity
+ * Big O Time Complexity: O(m*n) or Exponential Time Complexity
  * Big O Space Complexity: O(m + n) or Linear Space Complexity
  */
 
@@ -36,5 +36,7 @@ function howSum(targetSum, numbers) {
 console.log(howSum(7, [5, 3, 4, 7])); // should return  [3, 4] or [7]
 console.log(howSum(8, [2, 3, 5])); // should return [2, 2, 2, 2]  or [3, 5]
 console.log(howSum(7, [2, 4])); // should return null
+console.log(howSum(7, [3, 4])); // should return null
+console.log(howSum(7, [4, 3])); // should return null
 console.log(howSum(0, [1, 2, 3])); // []
 console.log(howSum(300, [1, 2, 3])); // [1, 1, 1, 1, 1, 1, 1, ... 293 more items]
