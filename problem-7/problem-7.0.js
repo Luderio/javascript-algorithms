530e9eea3d6a12ddb6e352ed47d73a13c8c20dbd
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-05 08:23:14 +0800

    Code Update: Added the base code on the problem-7.0.jd file

---
+++ problem-7/problem-7.0.js
@@ -0,0 +1,6 @@
+function howSum(targetSum, numbers) {
+  return [];
+}
+
+console.log(howSum(7, [5, 3, 4, 7])); // should return  [3, 4] or [7]
+console.log(howSum(8, [2, 3, 5])); // should return [2, 2, 2, 2]  or [3, 5]
