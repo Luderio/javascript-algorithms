b32fa45e88099027404dfae04d284c01347fe17b
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-05 09:44:25 +0800

    Code Update: Added the base code to the file

--- problem-7/problem-7.2.js
+++ problem-7/problem-7.2.js
@@ -1,9 +1,18 @@
 /**
- * Core Algorithm with Memoization
+ * Core Algorithm without Memoization
  *
  */
 
-function howSum(targetSum, numbers) {}
+function howSum(targetSum, numbers) {
+  // inout type checking.
+  if (typeof targetSum !== "number") {
+    throw new Error("argument should be a type of 'number'.");
+  }
+
+  if (!Array.isArray(numbers) || numbers === undefined) {
+    throw new Error("argument must be an array of numbers.");
+  }
+}
 
 console.log(howSum(7, [5, 3, 4, 7])); // should return  [3, 4] or [7]
 console.log(howSum(8, [2, 3, 5])); // should return [2, 2, 2, 2]  or [3, 5]
