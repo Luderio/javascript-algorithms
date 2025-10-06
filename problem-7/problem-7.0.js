9846abd8e9004a75f6b82020e59106268783fe87
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-06 19:11:30 +0800

    Modified the code

--- problem-7/problem-7.0.js
+++ problem-7/problem-7.0.js
@@ -38,5 +38,5 @@ console.log(howSum(8, [2, 3, 5])); // should return [2, 2, 2, 2]  or [3, 5]
 console.log(howSum(7, [2, 4])); // should return null
 console.log(howSum(7, [3, 4])); // should return null
 console.log(howSum(7, [4, 3])); // should return null
-console.log(howSum(0, [1, 2, 3])); // []
+console.log(howSum(0, [1, 2, 3])); // should return []
 console.log(howSum(300, [1, 2, 3])); // [1, 1, 1, 1, 1, 1, 1, ... 293 more items]
