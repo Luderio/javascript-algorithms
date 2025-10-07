2fb417242bd06f82727544aa911509c7b0a14d93
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-07 09:38:32 +0800

    Code Update: Added the base code for the coding problem.

---
+++ problem-8/problem-8.0.js
@@ -0,0 +1,6 @@
+function bestSum(targetSum, numbers) {
+  return numbers;
+}
+
+console.log(bestSum(7, [5, 3, 4, 7])); // should return [7]
+console.log(bestSum(8, [2, 3, 5])); // should return [3, 5]
