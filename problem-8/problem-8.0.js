dfd7161aacd30d31268e98baff717d2e445e1189
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-07 10:30:12 +0800

    Code Update: Completed the Core Algorithm

--- problem-8/problem-8.0.js
+++ problem-8/problem-8.0.js
@@ -1,6 +1,48 @@
+/**
+ * Core Algorithm without Memoization
+ * Time Complexity: O(n^m * m)
+ * Space Complexity: O(m * m) or O(m^2)
+ */
+
 function bestSum(targetSum, numbers) {
-  return numbers;
+  if (typeof targetSum !== "number") {
+    throw new Error("argument must be a type of 'number'");
+  }
+
+  if (!Array.isArray(numbers)) {
+    throw new Error("argument must be an array of numbers");
+  }
+
+  let combinations = null;
+
+  if (targetSum === 0) return [];
+  if (targetSum < 0) return null;
+
+  for (let number of numbers) {
+    let remainder = targetSum - number;
+    let result = bestSum(remainder, numbers);
+    if (result !== null) {
+      const combination = [...result, number];
+      if (combinations === null) combinations = combination;
+      if (combinations !== null && combination.length < combinations.length) {
+        combinations = combination;
+      }
+    }
+  }
+
+  return combinations;
 }
+
+/**
+ * Big O Notation
+ * m = targetSum
+ * n = numbers
+ *
+ * Time Complexity: O(n^m * m)
+ * Space Complexity: O(m * m) or O(m^2)
+ */
 
 console.log(bestSum(7, [5, 3, 4, 7])); // should return [7]
 console.log(bestSum(8, [2, 3, 5])); // should return [3, 5]
+console.log(bestSum(8, [1, 4, 5])); // should return [4, 4]
+console.log(bestSum(100, [1, 2, 5, 25])); // should return [4, 4]
