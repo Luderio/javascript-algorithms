f06166cd0ad2e4d3b53a3d31c2758a1fe613de33
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-08 20:10:57 +0800

    Code Update: Pushing initial code solution

--- problem-9/problem-9.0.js
+++ problem-9/problem-9.0.js
@@ -1,5 +1,5 @@
 /**
- * Core Algorithm without Optimization
+ * My Own Solution
  */
 
 function canConstruct(target, wordBank) {
@@ -11,6 +11,21 @@ function canConstruct(target, wordBank) {
   if (!Array.isArray(wordBank)) {
     throw new Error(`argument must be an array of strings`);
   }
+
+  if (target.length === 0 || target.trim() === "") return true;
+
+  const combinations = [];
+
+  for (let outer = 0; outer < wordBank.length; outer++) {
+    for (let inner = 0; inner < wordBank.length; inner++) {
+      const combination = `${wordBank[outer]}${wordBank[inner]}`;
+      if (target === combination) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // should return true
@@ -18,3 +33,4 @@ console.log(
   canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
 ); // should return false
 console.log(canConstruct("", ["dog", "cat", "mouse"])); // should return true
+console.log(canConstruct("dogdogcat", ["dog", "cat", "mouse"])); // should return false
