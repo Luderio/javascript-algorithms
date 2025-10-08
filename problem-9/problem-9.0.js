e9f26cb5859d6b49e070d9295be135296d57f2fa
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-08 19:31:45 +0800

    Code Update: Added input type checking

--- problem-9/problem-9.0.js
+++ problem-9/problem-9.0.js
@@ -3,10 +3,17 @@
  */
 
 function canConstruct(target, wordBank) {
-  return target;
+  // input type checking.
+  if (typeof target !== "string") {
+    throw new Error(`argument must be a type of 'String'`);
+  }
+
+  if (!Array.isArray(wordBank)) {
+    throw new Error(`argument must be an array of strings`);
+  }
 }
 
-console.log(canConstruct("abdcef", ["ab", "abc", "cd", "def", "abcd"])); // should return true
+console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // should return true
 console.log(
   canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
 ); // should return false
