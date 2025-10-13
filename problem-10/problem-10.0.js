0b2c0248b984face9df353af52c16530bed09500
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-13 12:10:03 +0800

    Completed the Core Algorith without Memoization code

--- problem-10/problem-10.0.js
+++ problem-10/problem-10.0.js
@@ -3,7 +3,6 @@
  */
 
 function countConstruct(target, wordBank) {
-  // input type checking
   // input type checking.
   if (typeof target !== "string") {
     throw new Error(`argument must be a type of 'String'`);
@@ -13,24 +12,42 @@ function countConstruct(target, wordBank) {
     throw new Error(`argument must be an array of strings`);
   }
 
-  return 0;
+  if (target.length === 0 || target === "") return 1;
+  let result = 0;
+
+  for (let item of wordBank) {
+    if (target.startsWith(item)) {
+      const new_target = target.replace(item, "");
+      //   console.log([item, target, new_target]);
+      result += countConstruct(new_target, wordBank);
+    }
+  }
+  return result;
 }
-console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // should return true
+
+/**
+ * m = target.length
+ * n = wordkBank.length
+ *
+ */
+
+console.log(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // should return 1
 console.log(
   countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
-); // should return false.
-console.log(countConstruct("", ["dog", "cat", "mouse"])); // should return true.
-// console.log(countConstruct("dogdogcat", ["dog", "cat", "mouse"])); // should return true.
-// console.log(
-//   countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
-// ); // should return true.
-// console.log(
-//   countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
-//     "e",
-//     "ee",
-//     "eee",
-//     "eeee",
-//     "eeeee",
-//     "eeeeee",
-//   ])
-// ); // should return false. slow
+); // should return 0.
+console.log(countConstruct("", ["dog", "cat", "mouse"])); // should return 1.
+console.log(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"])); // should return 2.
+console.log(countConstruct("dogdogcat", ["dog", "cat", "mouse"])); // should return 1.
+console.log(
+  countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"])
+); // should return 4.
+console.log(
+  countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", [
+    "e",
+    "ee",
+    "eee",
+    "eeee",
+    "eeeee",
+    "eeeeee",
+  ])
+); // should return 0. very slow
