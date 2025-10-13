cbb094fd7510a9bd9021b863bc6dfbb97aac5ee2
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-13 10:34:31 +0800

    Completed the Core Algorithm logic without optimization

--- problem-9/problem-9.1.js
+++ problem-9/problem-9.1.js
@@ -1,5 +1,5 @@
 /**
- * Core Algorithm withou Optimization
+ * Core Algorithm without Optimization
  */
 
 function canConstruct(target, wordBank) {
@@ -12,7 +12,18 @@ function canConstruct(target, wordBank) {
     throw new Error("argument must be an array of 'string'");
   }
 
-  return true;
+  //   console.log(target.length);
+  if (target.length === 0 || target === undefined) return true;
+
+  for (let item of wordBank) {
+    if (target.startsWith(item)) {
+      let new_target = target.replace(item, "");
+      let result = canConstruct(new_target, wordBank);
+      if (result === true) return true;
+    }
+  }
+
+  return false;
 }
 
 console.log(canConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"])); // should return true
@@ -20,4 +31,4 @@ console.log(
   canConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"])
 ); // should return false.
 console.log(canConstruct("", ["dog", "cat", "mouse"])); // should return true.
-console.log(canConstruct("dogdogcat", ["dog", "cat", "mouse"])); // should return false.
+console.log(canConstruct("dogdogcat", ["dog", "cat", "mouse"])); // should return true.
