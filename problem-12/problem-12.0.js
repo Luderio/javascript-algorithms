5b7bfc27c54b09387f39356c10a22fce319e3d97
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-15 07:22:20 +0800

    Code Update: Completed coding the core algorithm

--- problem-12/problem-12.0.js
+++ problem-12/problem-12.0.js
@@ -1,9 +1,29 @@
 function fibonacci(number) {
-  return number;
+  // input type checking.
+  if (typeof number !== "number") {
+    throw new Error("argument must be a type of 'string'");
+  }
+
+  const table = Array(number + 1).fill(0);
+  table[1] = 1;
+
+  for (let index = 0; index <= number; index++) {
+    table[index + 1] += table[index];
+    table[index + 2] += table[index];
+  }
+
+  return table[number];
 }
+
+/**
+ * n = number
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
 
 console.log(fibonacci(6)); // must return 8.
 console.log(fibonacci(7)); // must return 13.
 console.log(fibonacci(8)); // must return 21.
-console.log(fibonacci(50)); // must return 12586269025, will be slow.
-console.log(fibonacci(100)); // must return 354224848179262000000, will be slow.
+// console.log(fibonacci(50)); // must return 12586269025, will be slow.
+// console.log(fibonacci(100)); // must return 354224848179262000000, will be slow.
