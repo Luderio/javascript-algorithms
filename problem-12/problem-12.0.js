053b4b4cd6ea921835cc322654bd05f68fe48790
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-15 07:24:37 +0800

    Code Update: Added Comment

--- problem-12/problem-12.0.js
+++ problem-12/problem-12.0.js
@@ -1,3 +1,10 @@
+/**
+ * Core Iterative Algorithm without Tabulation.
+ * Unoptimized
+ * Big O Time Complexity: O(n)
+ * Big O Space Complexity: O(n)
+ */
+
 function fibonacci(number) {
   // input type checking.
   if (typeof number !== "number") {
@@ -25,5 +32,5 @@ function fibonacci(number) {
 console.log(fibonacci(6)); // must return 8.
 console.log(fibonacci(7)); // must return 13.
 console.log(fibonacci(8)); // must return 21.
-// console.log(fibonacci(50)); // must return 12586269025, will be slow.
+console.log(fibonacci(50)); // must return 12586269025, will be slow.
 // console.log(fibonacci(100)); // must return 354224848179262000000, will be slow.
