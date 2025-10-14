d183341b4dc4ec4d7b744109af1fc0671c59fd37
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-15 07:08:41 +0800

    Code Update: Added the base code on the file

---
+++ problem-12/problem-12.0.js
@@ -0,0 +1,9 @@
+function fibonacci(number) {
+  return number;
+}
+
+console.log(fibonacci(6)); // must return 8.
+console.log(fibonacci(7)); // must return 13.
+console.log(fibonacci(8)); // must return 21.
+console.log(fibonacci(50)); // must return 12586269025, will be slow.
+console.log(fibonacci(100)); // must return 354224848179262000000, will be slow.
