3007b930a22488a36cfb9ab161a2ce4470d6c984
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-15 08:39:50 +0800

    Code Update: Added the base code on the file

---
+++ problem-13/problem-13.0.js
@@ -0,0 +1,7 @@
+function gridTraveler(m, n) {}
+
+console.log(gridTraveler(2, 3)); // 3
+console.log(gridTraveler(4, 4)); // 20
+console.log(gridTraveler(6, 6)); // 252
+console.log(gridTraveler(10, 10)); // 48620
+console.log(gridTraveler(50, 50)); // 254,776,122,589,808,670,000,000,000,000.
