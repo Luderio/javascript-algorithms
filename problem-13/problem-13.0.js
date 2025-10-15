3c43aec6a2d79ede2926fea266c3fe6897426ebe
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-15 09:28:14 +0800

    Code Update: Completed the code

--- problem-13/problem-13.0.js
+++ problem-13/problem-13.0.js
@@ -1,4 +1,27 @@
-function gridTraveler(m, n) {}
+function gridTraveler(m, n) {
+  const table = Array(m + 1)
+    .fill()
+    .map(() => Array(n + 1).fill(0));
+  table[1][1] = 1;
+
+  for (let outer = 0; outer <= m; outer++) {
+    for (let inner = 0; inner <= n; inner++) {
+      const current = table[outer][inner]; // current position.
+      if (inner + 1 <= n) table[outer][inner + 1] += current; // increment right neighbor if the position is valid.
+      if (outer + 1 <= m) table[outer + 1][inner] += current; // increment down neighbor if the position is valid.
+    }
+  }
+
+  return table[m][n];
+}
+
+/**
+ * m = rows
+ * n = columns
+ *
+ * Time Complexity: O(mn)
+ * Space Complexity: O(mn)
+ */
 
 console.log(gridTraveler(2, 3)); // 3
 console.log(gridTraveler(4, 4)); // 20
