733450f22212304d3bc63db06b86e889e4ef502d
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-10-17 07:48:56 +0800

    Code Update: Added the base code on the file

--- problem-14/problem-14.0.js
+++ problem-14/problem-14.0.js
@@ -1,3 +1,9 @@
 /**
- * CanSum Tablation
+ * CanSum Core Algorithm with Tablation
  */
+
+function canSum(targetSum, numbers) {
+  return true;
+}
+
+console.log(canSum(7, [5, 3, 4, 7])); // should return true;
