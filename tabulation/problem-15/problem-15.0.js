5f4738322e94dfacc596bb3e6a5755d4e0a21632
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-11-03 10:17:13 +0800

    Code Update: Added the base code in the file

---
+++ tabulation/problem-15/problem-15.0.js
@@ -0,0 +1,19 @@
+/**
+ * howSum() Core algorithm implementation using Tabulation
+ */
+
+function howSum(targetSum, numbers) {}
+
+/**
+ * Big O Notation
+ *
+ * m = targetSum
+ * n = numbers
+ * Time Complexity:
+ * Space Complexity:
+ */
+
+howSum(7, [5, 3, 4, 7]); // should return  [3, 4] or [7]
+howSum(8, [2, 3, 5]); // should return [2, 2, 2, 2]  or [3, 5]
+howSum(7, [2, 4]); // should return null
+howSum(0, [1, 2, 3]); // []
