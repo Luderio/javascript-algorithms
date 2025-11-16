cc8482b5970b6429f2b783a337fbda58570d1920
Luderio Sanchez <luderio.rubis.sanchez@gmail.com>
2025-11-16 11:47:10 +0800

    Code Update

--- tabulation/problem-15/problem-15.0.js
+++ tabulation/problem-15/problem-15.0.js
@@ -47,5 +47,5 @@ function howSum(targetSum, numbers) {
 
 console.log(howSum(7, [5, 3, 4, 7])); // should return  [3, 4] or [7]
 console.log(howSum(8, [2, 3, 5])); // should return [2, 2, 2, 2]  or [3, 5]
-console.log(howSum(7, [2, 4])); // should return null
+console.log(howSum(7, [2, 4])); // should return null.
 console.log(howSum(0, [1, 2, 3])); // []
